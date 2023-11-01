cb5a2e9dd8bd3e13f059d8e4ed88c12bf4b0150e
Arcadia101 <camitu16@hotmail.com>
2023-11-01 13:58:57 -0500

    Pathfinding

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Grid : MonoBehaviour
 {
     Node[,] grid;
-    [SerializeField] int width = 25;
-    [SerializeField] int length = 25;
+    public int width = 25;
+    public int length = 25;
     [SerializeField] float cellSize = 1f;
     [SerializeField] LayerMask obstacleLayer;
+    [SerializeField] LayerMask terrainLayer;
     private void Awake() 
     {
         GenerateGrid();
@@ -27,7 +28,25 @@ public class Grid : MonoBehaviour
             }
         }
 
+        CalculateElevation();
         CheckPassable();
+    }
+
+    private void CalculateElevation()
+    {
+        for (int y = 0; y < width; y++)
+        {
+            for (int x = 0; x < length; x++)
+            {
+                Ray ray = new Ray(GetWorldPosition(x,y) + Vector3.up * 100f, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, float.MaxValue, terrainLayer))
+                {
+                    grid[x,y].elevation = hit.point.y;
+                }
+
+            }
+        }
     }
 
     private void CheckPassable()
@@ -38,7 +57,7 @@ public class Grid : MonoBehaviour
             {
                 Vector3 worldPos = GetWorldPosition(x,y);
                 bool passable = !Physics.CheckBox(worldPos, Vector3.one/2 * cellSize, Quaternion.identity, obstacleLayer);
-                grid[x,y] = new Node();
+                //grid[x,y] = new Node();
                 grid[x,y].passable = passable;
             }
         }
@@ -55,6 +74,24 @@ public class Grid : MonoBehaviour
             return false;
         }
         return true;
+    }
+
+    internal bool CheckBoundry(int posX, int posY)
+    {
+        if (posX < 0 || posX >= length)
+        {
+            return false;
+        }
+        if (posY < 0 || posY >= width)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public bool CheckWalkable(int pos_x, int pos_y)
+    {
+        return grid[pos_x,pos_y].passable;
     }
 
     public Vector2Int GetGridPos(Vector3 worldPos)
@@ -94,22 +131,33 @@ public class Grid : MonoBehaviour
     {
         if (grid == null)
         {
-            return;
-        }
-        for (int y = 0; y < width; y++)
-        {
-            for (int x = 0; x < length; x++)
+            for (int y = 0; y < width; y++)
             {
-                Vector3 pos = GetWorldPosition(x, y);
-                Gizmos.color = grid[x, y].passable ? Color.white : Color.red;
-                Gizmos.DrawCube(pos, Vector3.one/4);
+                for (int x = 0; x < length; x++)
+                {
+                    Vector3 pos = GetWorldPosition(x, y);
+                    Gizmos.DrawCube(pos, Vector3.one/4);
+                }
+            }
+        }
+        else
+        {
+            for (int y = 0; y < width; y++)
+            {
+                for (int x = 0; x < length; x++)
+                {
+                    Vector3 pos = GetWorldPosition(x, y, true);
+                    Gizmos.color = grid[x, y].passable ? Color.white : Color.red;
+                    Gizmos.DrawCube(pos, Vector3.one/4);
+                }
             }
         }
     }
 
-    private Vector3 GetWorldPosition(int x, int y)
+    public Vector3 GetWorldPosition(int x, int y, bool elevation = false)
     {
-        return new Vector3(transform.position.x + (x * cellSize), 0f, transform.position.z + (y * cellSize));
+        return new Vector3(transform.position.x + (x * cellSize), elevation == true ? grid[x,y].elevation : 0f, transform.position.z + (y * cellSize));
+        //return new Vector3(x * cellSize, 0f, y * cellSize);
     }
 }
 
--- Assets/Scripts/GridControl.cs
+++ Assets/Scripts/GridControl.cs
@@ -7,6 +7,16 @@ public class GridControl : MonoBehaviour
     [SerializeField] Grid targetGrid;
     [SerializeField] LayerMask terrainLayer;
 
+    Pathfinding pathfinding;
+    Vector2Int currentPosition = new Vector2Int();
+    List<PathNode> path;
+
+
+    private void Start() 
+    {
+        pathfinding = targetGrid.GetComponent<Pathfinding>();
+    }
+
     private void Update() 
     {
         if (Input.GetMouseButtonDown(0))
@@ -16,6 +26,10 @@ public class GridControl : MonoBehaviour
             if (Physics.Raycast(ray, out hit, float.MaxValue, terrainLayer))
             {
                 Vector2Int gridPos = targetGrid.GetGridPos(hit.point);
+
+                path = pathfinding.FindPath(currentPosition.x, currentPosition.y, gridPos.x, gridPos.y);
+                currentPosition = gridPos;
+                /*
                 GridObject gridObject = targetGrid.GetPlacedObject(gridPos);
                 if (gridObject == null)
                 {
@@ -25,7 +39,27 @@ public class GridControl : MonoBehaviour
                 {
                     Debug.Log("x= " + gridPos.x + " y= " + gridPos.y + gridObject.GetComponent<Character>().Name);
                 }
+                */
             }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (path == null)
+        {
+            Debug.Log("on draw is Null");
+            return;
+        }
+        if (path.Count == 0)
+        {
+            Debug.Log("on draw is 0");
+            return;
+        }
+        for (int i = 0; i < path.Count -1 ; i++)
+        {
+            Debug.Log("on draw is Drawing");
+            Debug.DrawLine(targetGrid.GetWorldPosition(path[i].pos_x, path[i].pos_y, true), targetGrid.GetWorldPosition(path[i + 1].pos_x, path[i + 1].pos_y, true));
         }
     }
 }
